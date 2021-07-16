ba23b734b4752e47b77410a43d55661febf3d81f
zyderus <zyderus@gmail.com>
2021-07-17 04:45:33 +0500

    dark theme toggle

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,32 +6,52 @@ import Button from './Button'
 const Navbar = () => {
   const [click, setClick] = useState(false)
   const [button, setButton] = useState(true)
+  const [isDark, setIsDark] = useState(false)
 
   const handleClick = () => setClick(!click)
   const closeMobileMenu = () => setClick(false)
   const showButton = () => (window.innerWidth <= 768 ? setButton(false) : setButton(true))
+  const toggleTheme = () => setIsDark(!isDark)
+
+  // Theme manipulation
+  const setTheme = (theme: any) => {
+    theme = theme ? 'dark' : ''
+    document.documentElement.className = theme
+    localStorage.setItem('theme', theme)
+  }
+  const getTheme = () => {
+    const theme: any = localStorage.getItem('theme')
+    setIsDark(theme)
+  }
 
   if (typeof window !== 'undefined') {
     window.addEventListener('resize', showButton)
   }
 
-  useEffect(() => {
-    showButton()
-  }, [])
+  useEffect(() => showButton(), [])
+  useEffect(() => getTheme(), [])
+  useEffect(() => setTheme(isDark), [isDark])
 
   return (
     <>
       <nav className={styles.navbar}>
-        <div>
+        <div className={styles.logoContainer}>
           <Link href='/'>
             <a className={styles.navbarLogo} onClick={closeMobileMenu}>
               RZ <i className='fab fa-typo3'></i>
             </a>
           </Link>
+          <div className={styles.themeContainer}>
+            <div className={styles.themeBtn} onClick={toggleTheme}>
+              {isDark ? 'dark' : 'light'}
+            </div>
+          </div>
+
           <div className={styles.menuIcon} onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
           </div>
         </div>
+
         <ul className={click ? styles.navMenu + ' ' + styles.active : styles.navMenu}>
           <li className={styles.navItem}>
             <Link href='/about'>
