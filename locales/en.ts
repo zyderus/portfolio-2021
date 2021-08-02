b72940b16148cb27022b012a6426e9cbcc867507
zyderus <zyderus@gmail.com>
2021-08-02 23:57:11 +0500

    update

---
+++ locales/en.ts
@@ -0,0 +1,30 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  about: 'About',
+  experience: 'Experience',
+  contact: 'Contact',
+  resume: 'Resume',
+  resumeLink: '/assets/resume/resume_en.pdf',
+
+  greet: 'Hi',
+  greetMorning: 'Good morning',
+  greetDay: 'Good afternoon',
+  greetEvening: 'Good evening',
+  intro: 'my name is',
+  firstName: 'Rustam',
+  lastName: 'Ziyadov',
+  slogan: 'and I build things with code',
+  aboutSummary:
+    'I am a Software Developer focusing in development, sometimes design, of functional and people-friendly applications. Mostly, I am about problem-solving, and still can appreciate a good joke.',
+  btnMessageMe: 'Message Me',
+
+  aboutMe: '.about().me()',
+  aboutMeTextP1:
+    'Hello! My name is Rustam and I enjoy making stuff that live on the web and run on mobiles. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque debitis inventore hic doloremque libero beatae!',
+  aboutMeTextP2:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, minima sed perferendis ab mollitia, doloribus eveniet officia, asperiores accusantium sunt tenetur placeat voluptate corporis? Minima ad temporibus laborum id facilis ut dignissimos deserunt ipsa voluptate recusandae placeat numquam dicta quasi nesciunt blanditiis doloremque assumenda.',
+
+  aboutMeTools: 'My Tools:',
+
+  experienceTitle: "Things I've built",
+}
