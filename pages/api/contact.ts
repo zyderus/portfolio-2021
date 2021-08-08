e27936a515fb8ddbc8baaaec3d6fe0c59ee6a704
zyderus <zyderus@gmail.com>
2021-08-08 17:05:09 +0500

    update i18n

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,7 +1,6 @@
 import nodemailer from 'nodemailer'
 import { NextApiRequest, NextApiResponse } from 'next'
 
-
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 465,
@@ -19,7 +18,7 @@ const sendEmail = async (req: NextApiRequest, res: NextApiResponse) => {
   const { name, email, message } = JSON.parse(req.body)
 
   const mailData = {
-    from: `Zyderus.Mailer 🚀 <${email}>`,
+    from: `Portfolio rings 🚀 <${email}>`,
     to: process.env.MY_EMAIL,
     subject: `${name} sent message from rystam.com`,
     text: `Contact form submission @ rystam.com from: ${name}, email: ${email}, message: ${message}`,
@@ -34,32 +33,10 @@ const sendEmail = async (req: NextApiRequest, res: NextApiResponse) => {
 
   try {
     const result = await transporter.sendMail(mailData)
-    // console.log('Message Sent', emailRes.response)
-    // transporter.close()
-    res.status(200).json(req.body)
-    // return result
+    res.status(200).json(result.response)
   } catch (err) {
-    // console.log('ERRor::', err)
-    // transporter.close()
     res.status(455).json(err)
-    // return err
   }
-
-  // res.status(200).json(req.body)
 }
 
 export default sendEmail
-
-
-// const transporter = nodemailer.createTransport({
-//   host: 'smtp.gmail.com',
-//   port: 465,
-//   secure: true,
-//   auth: {
-//       type: 'OAuth2',
-//       user: process.env.GOOGLE_USER,
-//       clientId: process.env.GOOGLE_ID,
-//       clientSecret: process.env.GOOGLE_SECRET,
-//       refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
-//   }
-// })
