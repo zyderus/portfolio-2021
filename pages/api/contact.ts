98fc213c2e24104a65e98768b5c396b8502f471e
zyderus <zyderus@gmail.com>
2021-08-08 11:48:12 +0500

    update contact form

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,18 +1,16 @@
 import nodemailer from 'nodemailer'
 import { NextApiRequest, NextApiResponse } from 'next'
 
-let transporter = nodemailer.createTransport({
+const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 465,
   secure: true,
   auth: {
       type: 'OAuth2',
-      user: process.env.GMAIL_USER,
-      clientId: process.env.GMAIL_ID,
-      clientSecret: process.env.GMAIL_SECRET,
-      refreshToken: process.env.GMAIL_REFRESH_TOKEN,
-      accessToken: process.env.GMAIL_ACCESS_TOKEN,
-      // expires: 3599
+      user: process.env.GOOGLE_USER,
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
   }
 });
 
@@ -20,10 +18,10 @@ const sendEmail = async (req: NextApiRequest, res: NextApiResponse) => {
   const { name, email, message } = JSON.parse(req.body)
 
   const mailData = {
-    from: 'email',
+    from: `Zyderus.Mailer 🚀 <${email}>`,
     to: process.env.MY_EMAIL,
     subject: `${name} sent message from rystam.com`,
-    // text: `Contact form submission @ rystam.com from: ${name}, email: ${email}, message: ${message}`,
+    text: `Contact form submission @ rystam.com from: ${name}, email: ${email}, message: ${message}`,
     html: `
       <p>Contact form submission @ rystam.com</p>
       <p><strong>Name: </strong> ${name} </p>
@@ -34,14 +32,16 @@ const sendEmail = async (req: NextApiRequest, res: NextApiResponse) => {
   }
 
   try {
-    const emailRes = await transporter.sendMail(mailData)
-    console.log('Message Sent', emailRes.response)
+    const result = await transporter.sendMail(mailData)
+    // console.log('Message Sent', emailRes.response)
     // transporter.close()
     res.status(200).json(req.body)
+    // return result
   } catch (err) {
-    console.log('ERRor::', err)
+    // console.log('ERRor::', err)
     // transporter.close()
     res.status(455).json(err)
+    // return err
   }
 
   // res.status(200).json(req.body)
